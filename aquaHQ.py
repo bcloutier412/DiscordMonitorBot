471a18d8f87c0f0b0647d7bf3d4d22f8394ccf53
bcloutier412 <brandoncloutier412@gmail.com>
2022-03-22 18:38:20 -0700

    multi server monitor

--- aquaHQ.py
+++ aquaHQ.py
@@ -1,22 +1,22 @@
-
 from servercollection import aquaHQ
 from requestsfunc import retrieve_messages, send_message
 
 recent_messages = []
 for value in aquaHQ.channelCollection:
     recent_messages.append('')
+def run():
+    while True:
+        counter = 0
+        for value in aquaHQ.channelCollection:
+            json_obj = retrieve_messages(aquaHQ.activeToken ,value['channel'])
+            try:
+                payload_content_0 = json_obj[0]['content']
+            except KeyError:
+                print('KeyError 0')
 
-while True:
-    counter = 0
-    for value in aquaHQ.channelCollection:
-        json_obj = retrieve_messages(aquaHQ.activeToken ,value['channel'])
-        try:
-            payload_content_0 = json_obj[0]['content']
-        except KeyError:
-            print('KeyError 0')
-
-        if payload_content_0 != recent_messages[counter]:
-            send_message(value['directedChannel'], payload_content_0, aquaHQ.proxyToken)
-            recent_messages[counter] = payload_content_0
-        counter += 1
-    counter = 0
+            if payload_content_0 != recent_messages[counter]:
+                send_message(value['directedChannel'], payload_content_0, aquaHQ.proxyToken)
+                recent_messages[counter] = payload_content_0
+            counter += 1
+        counter = 0
+run()
